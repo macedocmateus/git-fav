4583192679f75ecde242957646ae464449392aff
Mateus <mateusmacedoeng@gmail.com>
2024-03-28 00:39:04 -0300

    add user into local storage

--- source/favorites.js
+++ source/favorites.js
@@ -19,15 +19,42 @@ export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
-
-        GithubUser.search('macedocmateus').then(user => console.log(user) )
     }
 
     load() {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
 
-        console.log(this.entries);
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
 
+
+    async add(username) {
+        try {
+
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if (userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+                
+            
+
+            const user = await GithubUser.search(username)
+    
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrador!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
     }
 
     delete(user) {
@@ -36,14 +63,15 @@ export class Favorites {
 
         this.entries = filteredEntries
         this.update()
-        
-        
+        this.save()
     }
 
     onadd() {
         const addButton = this.root.querySelector('.search button')
         addButton.onclick = () => {
-            const value = this.root.querySelector('.search input')
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
         }
     }
 }
